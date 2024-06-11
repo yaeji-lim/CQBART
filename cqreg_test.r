0be75a358be7d31687c16df6aedd31adde09dbf7
yaeji-lim <58451641+yaeji-lim@users.noreply.github.com>
2024-06-11 10:31:42 -0400

    Create cqreg_test.r

---
+++ cqreg_test.r
@@ -0,0 +1,17 @@
+library(Rcpp)
+library(RcppEigen)
+library(MASS)
+library(QuantPsyc)
+library(RcppArmadillo)
+
+data(Boston)
+names(Boston)
+y = log(Boston$medv)
+y = y- mean(y)
+X = Boston[,-14]
+X =Make.Z(X)
+
+
+sourceCpp("test.cpp")
+
+fit=cqr_lasso(X, y, K=9)
